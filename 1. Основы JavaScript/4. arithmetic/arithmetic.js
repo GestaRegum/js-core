75cd2650f352d3851c50806e5608587ff2038ba1
Денисов Виталий <147824834+GestaRegum@users.noreply.github.com>
2024-04-16 15:57:51 +0000

    it's

--- 1. Основы JavaScript/4. arithmetic/arithmetic.js
+++ 1. Основы JavaScript/4. arithmetic/arithmetic.js
@@ -1,4 +1,17 @@
 const arithmetic = (a, b, operator) => {
+  switch (operator) {
+    case "add":
+      return a + b;
+    case "subtract":
+      return a - b;
+    case "multiply":
+      return a * b;
+    case "divide":
+      return a / b;
+    default: {
+      return NaN;
+    }
+  }
   // Пишите код здесь
 };
 
