118e6956b883d3a5dfa315bd70275f868f5a5a21
Денисов Виталий <147824834+GestaRegum@users.noreply.github.com>
2024-04-16 10:02:31 +0000

    готово

--- 1. Основы JavaScript/2. finalGrade/finalGrade.js
+++ 1. Основы JavaScript/2. finalGrade/finalGrade.js
@@ -1,5 +1,14 @@
 function finalGrade(exam, projects) {
   // Пишите код здесь
+  if (exam > 90 || projects > 10) {
+    return 100;
+  } else if (exam > 75 && projects >= 5) {
+    return 90;
+  } else if (exam > 50 && projects >= 2) {
+    return 75;
+  } else {
+    return 0;
+  }
 }
 
 export { finalGrade };
