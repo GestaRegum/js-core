e15b47b76dd31f999879123d68dc401a32ff6f33
Денисов Виталий <147824834+GestaRegum@users.noreply.github.com>
2024-04-24 18:41:14 +0000

    it's

--- 1. Основы JavaScript/8. getLettersCount/getLettersCount.js
+++ 1. Основы JavaScript/8. getLettersCount/getLettersCount.js
@@ -1,6 +1,17 @@
 function getLettersCount(str) {
-  // Пишите код здесь
-}
+  let calc = {};
+  let letter = str.toLowerCase();
+  for (let i = 0, l = str.length; i < l; i++) {
+    let smallLetter = letter[i];
 
+    if (!calc[smallLetter]) {
+      calc[smallLetter] = 0;
+    }
+
+    calc[smallLetter]++;
+  }
+
+  return calc;
+}
 export { getLettersCount };
 // Для запуска теста вводим в терминале команду: npm run test:current -- getLettersCount.test.js
