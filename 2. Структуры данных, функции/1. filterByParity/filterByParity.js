5c7619181dd17d48d0c70a22bace6062f1266578
Денисов Виталий <147824834+GestaRegum@users.noreply.github.com>
2024-05-07 18:41:11 +0000

    2.1 & 2.2

--- 2. Структуры данных, функции/1. filterByParity/filterByParity.js
+++ 2. Структуры данных, функции/1. filterByParity/filterByParity.js
@@ -1,4 +1,30 @@
 const filterByParity = (numbers, parity) => {
+ let evenOrOdd = [];
+
+  if (numbers === '') {
+    return numbers;
+  }
+ 
+  
+  if (parity === 'even') {
+    for (let i = 0; i < numbers.length; i++) {
+      if (numbers[i] % 2 === 0) {
+        evenOrOdd.push(numbers[i]);
+      } 
+      console.log(evenOrOdd)
+    }
+  }
+  
+  if (parity === 'odd') {
+    for (let i = 0; i < numbers.length; i++) {
+      if (numbers[i] % 2 !== 0) {
+        evenOrOdd.push(numbers[i]) ;
+        } 
+      console.log(evenOrOdd)
+    }
+  }
+
+return evenOrOdd;
   // Пишите код здесь
 };
 
--- 2. Структуры данных, функции/2. findAllIndices/findAllIndices.js
+++ 2. Структуры данных, функции/2. findAllIndices/findAllIndices.js
@@ -1,6 +1,14 @@
 function findAllIndices(arr, value) {
-  // Пишите код здесь
-}
+  let evenOrOdd = [];
+
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === value ) {
+        evenOrOdd.push(i);
+      } 
+      console.log(evenOrOdd)
+    }
+    return evenOrOdd
+};
 
 export { findAllIndices };
 // Для запуска теста вводим в терминале команду: npm run test:current -- findAllIndices.test.js
