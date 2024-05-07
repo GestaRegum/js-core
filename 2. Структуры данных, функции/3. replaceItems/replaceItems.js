db4f024981f1d4df73bfb6c5ab075846b11f36b2
Денисов Виталий <147824834+GestaRegum@users.noreply.github.com>
2024-05-07 19:40:03 +0000

    on

--- 2. Структуры данных, функции/3. replaceItems/replaceItems.js
+++ 2. Структуры данных, функции/3. replaceItems/replaceItems.js
@@ -1,10 +1,27 @@
 function replaceItemsClear(arr, item, replaceItem) {
+  let notReplaceItem = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== item) {
+      notReplaceItem.push(arr[i]);
+    } else (notReplaceItem.push(replaceItem))
+  }
+  return notReplaceItem
   // Пишите код здесь
 }
 
 function replaceItemsMutate(arr, item, replaceItem) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === item) {
+      arr[i] = replaceItem;
+      
+    }
+  }
+  console.log(arr)
+return arr;
   // Пишите код здесь
 }
+
 
 export { replaceItemsClear, replaceItemsMutate };
 // Для запуска теста вводим в терминале команду: npm run test:current -- replaceItems.test.js
