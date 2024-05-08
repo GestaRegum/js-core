af7180337e763d7b74d738813599543ec5ae36c6
Денисов Виталий <147824834+GestaRegum@users.noreply.github.com>
2024-05-08 12:04:36 +0000

    eeeee

--- 2. Структуры данных, функции/4. partition/partition.js
+++ 2. Структуры данных, функции/4. partition/partition.js
@@ -1,5 +1,30 @@
 function partition(array, callback) {
-  // Пишите код здесь
+
+  let trueLine = [];
+  let falseLine = [];
+
+  if (callback === undefined) {
+    for (let i = 0; i < array.length; i++) {
+      if (Boolean(array[i]) === true) {
+        trueLine.push(array[i])
+      } else {
+        falseLine.push(array[i]);
+      }
+    }
+    return [trueLine, falseLine]
+  } else {
+
+    for (let i = 0; i < array.length; i++) {
+    if (Boolean(callback(array[i])) === true) {
+        trueLine.push(array[i]);
+      } else {
+        falseLine.push(array[i]);
+      }
+    }
+ 
+return [trueLine, falseLine]
+
+  }
 }
 
 export { partition };
