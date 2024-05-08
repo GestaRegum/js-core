83cd3f9589e6e6ee5b4d40edb74c2584a3731673
Денисов Виталий <147824834+GestaRegum@users.noreply.github.com>
2024-05-08 21:11:47 +0000

    gh

--- 2. Структуры данных, функции/5. reverseLongWords/reverseLongWords.js
+++ 2. Структуры данных, функции/5. reverseLongWords/reverseLongWords.js
@@ -1,4 +1,21 @@
 const reverseLongWords = (str, n) => {
+  let strWord = str.split(' ');
+  let reversWord = [];
+  let newWord = [];
+
+console.log(strWord)
+
+
+  for (let i = 0; i < strWord.length; i++) {
+    if (strWord[i].length >= n) {
+       newWord = strWord[i].split('').reverse().join('');
+    } else {
+      newWord = strWord[i];
+    }
+   reversWord.push(newWord);
+    }
+
+return reversWord.join(' ');
   // Пишите код здесь
 };
 
