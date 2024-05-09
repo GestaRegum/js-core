7d846875d7f8e17f7c1976c6d4be32d12ffbaef3
Денисов Виталий <147824834+GestaRegum@users.noreply.github.com>
2024-05-09 16:39:42 +0000

    8 задача

--- 2. Структуры данных, функции/8. wrapInParagraph/wrapInParagraph.js
+++ 2. Структуры данных, функции/8. wrapInParagraph/wrapInParagraph.js
@@ -1,4 +1,22 @@
 function wrapInParagraph(text) {
+ let textTest = [];
+
+  let wordOnText = text.split('\u000A');
+  console.log(wordOnText);
+  if (wordOnText.length === 1) {
+    textTest.push('<p>' + wordOnText + '</p>');
+    return textTest.join()
+  } else {
+
+  for (let i = 0; i < wordOnText.length; i++) {
+    textTest.push('<p>' + wordOnText[i] + '</p>');
+    console.log(textTest);
+    
+  }
+
+}
+ return textTest.join('\u000A');
+
   // Пишите код здесь
 }
 
