4f43fd1032455d5cb96268f907fd8140bbaaade7
Денисов Виталий <147824834+GestaRegum@users.noreply.github.com>
2024-05-11 17:29:40 +0000

    11 zadahca

--- 2. Структуры данных, функции/11. arrayFilters/arrayFilters.js
+++ 2. Структуры данных, функции/11. arrayFilters/arrayFilters.js
@@ -1,14 +1,23 @@
 const inRange = (a, b) => {
-  // Пишите код здесь
+return function (x) {
+  return a <= x && b >= x
+}
+// Пишите код здесь
 };
 
 const inArray = (arr) => {
+ return function(item) {
+ return arr.includes(item)
+ }
   // Пишите код здесь
 };
 
 const notInArray = (arr) => {
+  return function(item) {
+   return !arr.includes(item)
+    
   // Пишите код здесь
 };
-
+}
 export { inArray, inRange, notInArray };
 // Для запуска теста вводим в терминале команду: npm run test:current -- arrayFilters.test.js
