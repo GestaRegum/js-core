cac98bcb9304582d2720b53ccb1b8f144648eee3
Денисов Виталий <147824834+GestaRegum@users.noreply.github.com>
2024-05-13 19:29:41 +0000

    x_X

--- 2. Структуры данных, функции/13. sequenceSum/sequenceSum.js
+++ 2. Структуры данных, функции/13. sequenceSum/sequenceSum.js
@@ -1,4 +1,14 @@
 export const sequenceSum = (begin, end) => {
+    if (begin === end) {
+      return end;
+    } else if (begin < end || (isNaN(begin) && isNaN(end))){
+       return +end + sequenceSum (begin, end - 1);
+    } else {
+      return NaN
+   }
+
+  
+
   // Пишите код здесь
 };
 // Для запуска теста вводим в терминале команду: npm run test:current -- sequenceSum.test.js
