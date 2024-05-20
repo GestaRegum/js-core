f0fc804886fc2daf4b48fe9802785abdda936b18
Денисов Виталий <147824834+GestaRegum@users.noreply.github.com>
2024-05-20 12:05:30 +0000

    ураааа

--- 2. Структуры данных, функции/17. once/once.js
+++ 2. Структуры данных, функции/17. once/once.js
@@ -1,8 +1,26 @@
 const once = (fn) => {
-  console.log(fn)
+ 
+  let firstCall = false;
+  let value;
+  return function () {
+    if (!firstCall) {
+      firstCall = true;
+      value = fn.apply(this, arguments);
+    } else if (firstCall) {
+      return value;
+      } else {
+        try {
+          value = fn.apply(this, arguments);
+        } catch (err) {
+          
+        }
+    
+  };
+  return value;
+}
   
   // Пишите код здесь
 };
-
+ 
 export { once };
 // Для запуска теста вводим в терминале команду: npm run test:current -- once.test.js
