b6884d0e4b818d0cb4560acf3f852f78ba5b2087
Денисов Виталий <147824834+GestaRegum@users.noreply.github.com>
2024-06-18 19:16:38 +0000

    изм

--- 3. Прототипы, классы/11. personalAccount/personalAccount.js
+++ 3. Прототипы, классы/11. personalAccount/personalAccount.js
@@ -1,75 +1,66 @@
 class Person {
-  constructor (firstName, secondName, birthDay) {
-    this.firstName = firstName;
-    this.secondName = secondName;
-    this.birthDay = birthDay;
+  constructor(firstName, secondName, birthDay) {
+      this.firstName = firstName;
+      this.secondName = secondName;
+      this.birthDay = birthDay;
   }
-  
-  getAge () {
-    let toDay = new Date('2023-05-23');
-    let birthData = new Date(this.birthDay);  
-    let year = toDay.getFullYear() - birthData.getFullYear();
-    return year
+
+  getAge() {
+      let toDay = new Date("2023-05-23");
+      let birthData = new Date(this.birthDay);
+      let year = toDay.getFullYear() - birthData.getFullYear();
+      return year;
   }
-  
+
   get fullName() {
-    return `${this.firstName} ${this.secondName}`;
+      return `${this.firstName} ${this.secondName}`;
   }
-  
-  
-    // Пишите код здесь
-  }
-  
-  class Account {
-  
-    constructor(person, sum) {    
+
+  // Пишите код здесь
+}
+
+class Account {
+  constructor(person, sum) {
+      this.person = person;
       this.balans = sum;
-      this.history = []
-    }
-  
+      this.history = [];
+  }
+
   static transfer(fromAccount, toAccount, amount) {
-    fromAccount.withdrawMoney(amount, toAccount);
-    toAccount.addMoney(amount, fromAccount)
-  
-  
+      fromAccount.withdrawMoney(amount, toAccount);
+      toAccount.addMoney(amount, fromAccount);
   }
-  
+
   addMoney(amount, description) {
-    console.log(this.balans)
-  this.balans += amount;
-  this.history.push({
-        timestamp: Date.now(),
-        target: 'in',
-        amount: amount,
-        description: description
-  
-  })
+      this.balans += +amount;
+      this.history.push({
+          timestamp: Date.now(),
+          target: "in",
+          amount: amount,
+          description: description,
+      });
   }
-  
+
   withdrawMoney(amount, description) {
-  this.balans -= amount;
-  this.history.push({
-        timestamp: Date.now(),
-        target: 'out',
-        amount: amount,
-        description: description
-      })
+      this.balans -= amount;
+      this.history.push({
+          timestamp: Date.now(),
+          target: "out",
+          amount: amount,
+          description: description,
+      });
   }
-  
-  getAmount () {
-    return this.balans
+
+  getAmount() {
+      return this.balans;
   }
-  
+
   getAccountHistory() {
-    return this.history
+      return this.history;
   }
-  
-  
-  
-    // Пишите код здесь
-  }
-  
-  
-  export { Person, Account };
-  // npm run test:current -- personalAccount.test.js
-  
+
+  // Пишите код здесь
+}
+
+export { Person, Account };
+// npm run test:current -- personalAccount.test.js
--- 3. Прототипы, классы/8. isEmpty/isEmpty.js
+++ 3. Прототипы, классы/8. isEmpty/isEmpty.js
@@ -1,18 +1,15 @@
 function isEmpty(obj) {
   for (let key in obj) {
-    if (obj.hasOwnProperty(key)) {
-        return false;
-    }
-}
-return true;
+      if (obj.hasOwnProperty(key)) {
+          return false;
+      }
+  }
+  return true;
   // Пишите код здесь
 }
 
 function isEmptyWithProtos(obj) {
-
-  return obj.__proto__ === undefined
- 
-  
+  return (obj.__proto__ === undefined && obj.__proto__ !== null);
 
   // Пишите код здесь
 }
