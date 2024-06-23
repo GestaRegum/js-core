5ed45607e15b4135665a7c0526803e6d919c16c4
Денисов Виталий <147824834+GestaRegum@users.noreply.github.com>
2024-06-23 20:28:17 +0000

    4.1

--- 4. Асинхронность и запросы/1. arrayToCsv/arrayToCsv.js
+++ 4. Асинхронность и запросы/1. arrayToCsv/arrayToCsv.js
@@ -1,6 +1,26 @@
 function arrayToCsv(data) {
-  // Пишите код здесь
+  try {
+      const escapeString = (param) => {
+          if (typeof param !== "string" && typeof param !== "number") {
+              throw new Error("Unexpected value");
+          }
+
+          if (typeof param === "string") {
+              if (param.includes(",") || param.includes('"')) {
+                param = param.split('"').join('""');
+                param = `"${param}"`;
+              }
+          }
+          return param;
+      };
+
+      const csvRows = data.map((row) => row.map(escapeString).join(","));
+      const csvString = csvRows.join("\n");
+      return csvString;
+  } catch (err) {
+      throw err.message;
+  }
 }
 
 export { arrayToCsv };
-// Для запуска теста вводим в терминале команду: npm run test:current -- csvGenerator.test.js
+// Для запуска теста вводим в терминале команду: npm run test:current -- arrayToCsv.test.js
